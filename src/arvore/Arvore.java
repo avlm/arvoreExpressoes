c64c5573cca0a6876037261a3cf36692a313b8c6
Victor Luckwu <avlm@ecomp.poli.br>
2016-11-16 20:16:45 -0300

    Criação do método inserir

--- src/arvore/Arvore.java
+++ src/arvore/Arvore.java
@@ -1,18 +1,31 @@
 package arvore;
 
 public class Arvore implements IArvore {
+	
+	private TreeNode raiz;
 
-	public void inserir() {
-		// TODO Auto-generated method stub
+	public void inserir(TreeNode novo) {
+		
+		if (raiz == null) {
+			raiz = novo;
+		} else {
+			TreeNode aux = raiz;
+			
+			if (aux.getRight() == null) {
+				aux.setRight(novo);
+			} else {
+				aux.setLeft(novo);
+			}
+		}
 		
 	}
 
-	public boolean remover() {
+	public boolean remover(char info) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public boolean existe() {
+	public boolean existe(char info) {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -25,6 +38,14 @@ public class Arvore implements IArvore {
 	public void percorrePosOrdem() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public TreeNode getRaiz() {
+		return raiz;
+	}
+	
+	public void setRaiz(TreeNode raiz) {
+		this.raiz = raiz;
 	}
 
 }

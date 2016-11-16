1c9d25702695cd8eee4c6951fa87d403c67ebcd0
Victor Luckwu <avlm@ecomp.poli.br>
2016-11-16 20:17:48 -0300

    iniciada a implementação do método montarArvore

--- src/arvore/ArvoreExpressao.java
+++ src/arvore/ArvoreExpressao.java
@@ -1,9 +1,49 @@
 package arvore;
 
-public class ArvoreExpressao extends Arvore {
+import java.util.Stack;
+
+public class ArvoreExpressao {
+	
+	Stack<TreeNode>  pilhaExp = new Stack();
+	Stack<Character> pilhaOp  = new Stack();
 	
 	public void montarArvore(String exp) {
 		
+		char[] expressao = exp.toCharArray();
+		Arvore arvExp = new Arvore();
+		
+		for (char c : expressao) {
+			switch (c) {
+				case '+':
+					pilhaOp.push(c);
+					break;
+				case '-':
+					pilhaOp.push(c);
+					break;
+				case '/':
+					pilhaOp.push(c);
+					break;
+				case '*':
+					pilhaOp.push(c);
+					break;
+				case ')':
+					/** 
+					 * retira um operador da pilhaOp para ser raiz,
+					 * o primeiro node da pilhaExp vai ser o filho da direita
+					 * e o segundo vai ser o filho da esquerda.
+					 * empilha a arvore em pilhaExp.
+					 */
+					arvExp.setRaiz(null);
+					break;
+				default:
+					if (Character.isLetterOrDigit(c)) {
+						TreeNode novo = new TreeNode(c);
+						arvExp.inserir(novo);
+					} else {
+						throw new RuntimeException("Sua Expressão possui um caractere inválido!");
+					}
+			}
+		}
 	}
 	
 	public void substituirVariaveis() {

76714a377a2c6fdeeca5f56860b358a4e2925d30
Victor Luckwu <avlm@ecomp.poli.br>
2016-11-16 20:18:35 -0300

    correção

--- src/arvore/IArvore.java
+++ src/arvore/IArvore.java
@@ -1,9 +1,9 @@
 package arvore;
 
 public interface IArvore {
-	public void inserir();
-	public boolean remover();
-	public boolean existe();
+	public void inserir(TreeNode novo);
+	public boolean remover(char info);
+	public boolean existe(char info);
 	public void percorreEmOrdem();
 	public void percorrePosOrdem();
 }
